2a9d37c87e790ec337edcae54a14da9ebb369e4b
Kevin Abatan <abatan.k@gmail.com>
2025-08-02 15:06:52 +0200

    feat: adding wip create & findOne implementation

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { writeFile } from "node:fs/promises";
 import { join as nodeJoin } from "node:path";
 import { type AdapterDebugLogs, createAdapter } from "better-auth/adapters";
 import type { Client } from "gel";
-import { join, map, merge, pipe, values } from "remeda";
+import { capitalize, concat, join, keys, map, mapToObj, merge, pipe, values } from "remeda";
+import { match } from "ts-pattern";
 import { generateFieldsString } from "./utils";
 
 interface GelAdapterConfig {
@@ -21,66 +22,142 @@ interface GelAdapterConfig {
   moduleName?: string;
 }
 
-export const gelAdapter = (_: Client, config: GelAdapterConfig = { moduleName: "auth" }) =>
+export const gelAdapter = (db: Client, config: GelAdapterConfig) =>
   createAdapter({
     config: {
       adapterId: "gel-adapter",
       adapterName: "Gel Adapter",
       usePlural: config.usePlural ?? false,
       debugLogs: config.debugLogs ?? false,
+      disableIdGeneration: true,
       supportsJSON: true,
       supportsDates: true,
       supportsBooleans: true,
       supportsNumericIds: false,
     },
-    adapter: () => ({
-      create: async () => {
-        throw new Error("Not implemented");
-      },
-      update: async () => {
-        throw new Error("Not implemented");
-      },
-      updateMany: async () => {
-        throw new Error("Not implemented");
-      },
-      delete: async () => {
-        throw new Error("Not implemented");
-      },
-      count: async () => {
-        throw new Error("Not implemented");
-      },
-      findOne: async () => {
-        throw new Error("Not implemented");
-      },
-      findMany: async () => {
-        throw new Error("Not implemented");
-      },
-      deleteMany: async () => {
-        throw new Error("Not implemented");
-      },
-      createSchema: async ({ tables, file = `./dbschema/${config.moduleName}.gel` }) => {
-        let rootScalarEnumTypes: Record<Capitalize<string>, string> = {};
-        const schema = pipe(
-          values(tables),
-          map(({ modelName, fields }) => {
-            const { scalarEnumTypes, fields: fieldString } = generateFieldsString(fields);
-            rootScalarEnumTypes = merge(rootScalarEnumTypes, scalarEnumTypes);
-            return `  type ${modelName} {\n    ${fieldString}\n  };`;
-          }),
-          join("\n\n"),
-          (str) => {
-            const scalarEnumTypesString =
-              values(rootScalarEnumTypes).length > 0
-                ? `  ${values(rootScalarEnumTypes).join("\n  ")}\n\n`
-                : "";
+    adapter: (options) => {
+      return {
+        create: async ({ data, model, select }) => {
+          const query = `
+select (
+  insert ${config.moduleName ?? "default"}::${model} {
+    ${pipe(
+      keys(data) as string[],
+      map((key) => {
+        console.log("[Create] Key: ", key);
+        const fieldAttributes = options.getFieldAttributes({ model, field: key });
+        //TODO: Create method which return the fieldAttributes depending on the key or the fieldName
+        return `${key} := <${match(fieldAttributes.type)
+          .with("string", () => "str")
+          .with("number", () => "int")
+          .with("boolean", () => "bool")
+          .with("date", () => "datetime")
+          .with("string[]", () => "array<str>")
+          .with("number[]", () => "array<int>")
+          .otherwise(() => capitalize(fieldAttributes.fieldName ?? key))}>$${key}`;
+      }),
+      join(",\n    "),
+    )}
+  }
+) {
+  ${pipe(select ?? concat(keys(data) as string[], ["id"]), join(",\n  "))}
+}`;
 
-            return `module ${config.moduleName} {\n\n${scalarEnumTypesString}${str}\n}\n`;
-          },
-        );
+          return await db.queryRequiredSingle(query, data);
+        },
+        update: async () => {
+          throw new Error("Not implemented");
+        },
+        updateMany: async () => {
+          throw new Error("Not implemented");
+        },
+        delete: async () => {
+          throw new Error("Not implemented");
+        },
+        count: async () => {
+          throw new Error("Not implemented");
+        },
+        findOne: async ({ model, where, select }) => {
+          const fieldKeys = keys(options.schema[model]?.fields ?? {});
 
-        await writeFile(nodeJoin(process.cwd(), file), schema);
+          const whereClause = pipe(
+            where,
+            map(({ connector, field, operator }, index) => {
+              console.log("[Where Clause] Field: ", field);
+              const fieldAttributes = options.getFieldAttributes({ model, field });
 
-        return { path: file, append: false, overwrite: true, code: schema };
-      },
-    }),
+              return pipe(
+                index === 0 ? "" : connector === "AND" ? "and " : "or ",
+                (str) =>
+                  `${str}.${fieldAttributes.fieldName ?? field} ${match(operator)
+                    .with("eq", () => "=")
+                    .with("ne", () => "!=")
+                    .with("lt", () => "<")
+                    .with("lte", () => "<=")
+                    .with("gt", () => ">")
+                    .with("gte", () => ">=")
+                    .with("in", () => "in")
+                    .with("contains", () => "like")
+                    .with("starts_with", () => "like")
+                    .with("ends_with", () => "like")
+                    .exhaustive()} <${
+                    field === "id"
+                      ? "uuid"
+                      : match(fieldAttributes.type)
+                          .with("string", () => "str")
+                          .with("number", () => "int")
+                          .with("boolean", () => "bool")
+                          .with("date", () => "datetime")
+                          .with("string[]", () => "array<str>")
+                          .with("number[]", () => "array<int>")
+                          .otherwise(() => capitalize(fieldAttributes.fieldName ?? field))
+                  }>$${fieldAttributes.fieldName ?? field}`,
+              );
+            }),
+            join("\n"),
+          );
+
+          const query = `
+select ${config.moduleName ?? "default"}::${model} {
+  ${pipe(select ?? fieldKeys, join(",\n  "))},
+} filter ${whereClause} limit 1`;
+
+          options.debugLog("[Find One] Query: ", query);
+
+          const data = mapToObj(where, (v) => [v.field, v.value]);
+
+          return await db.queryRequiredSingle(query, data);
+        },
+        findMany: async () => {
+          throw new Error("Not implemented");
+        },
+        deleteMany: async () => {
+          throw new Error("Not implemented");
+        },
+        createSchema: async ({ tables, file = `./dbschema/${config.moduleName}.gel` }) => {
+          let rootScalarEnumTypes: Record<Capitalize<string>, string> = {};
+          const schema = pipe(
+            values(tables),
+            map(({ modelName, fields }) => {
+              const { scalarEnumTypes, fields: fieldStr } = generateFieldsString(fields);
+              rootScalarEnumTypes = merge(rootScalarEnumTypes, scalarEnumTypes);
+              return `  type ${modelName} {\n    ${fieldStr}\n  };`;
+            }),
+            join("\n\n"),
+            (schemaStr) => {
+              const scalarEnumTypesString =
+                values(rootScalarEnumTypes).length > 0
+                  ? `  ${values(rootScalarEnumTypes).join("\n  ")}\n\n`
+                  : "";
+
+              return `module ${config.moduleName} {\n\n${scalarEnumTypesString}${schemaStr}\n}\n`;
+            },
+          );
+
+          await writeFile(nodeJoin(process.cwd(), file), schema);
+
+          return { path: file, append: false, overwrite: true, code: schema };
+        },
+      };
+    },
   });
--- tests/gel-adapter.spec.ts
+++ tests/gel-adapter.spec.ts
@@ -1,23 +1,49 @@
 import { runAdapterTest } from "better-auth/adapters/test";
 import { createClient } from "gel";
-import { describe } from "vitest";
+import { beforeAll, describe } from "vitest";
 import { gelAdapter } from "../src";
 
 describe("Gel Adapter Tests", async () => {
   const db = createClient();
   const adapter = gelAdapter(db, {
+    moduleName: "auth",
     debugLogs: {
       isRunningAdapterTests: true,
     },
   });
 
-  // beforeAll(async () => {
-  //   await db.execute("delete session;");
-  //   await db.execute("delete account;");
-  //   await db.execute("delete user;");
-  // });
+  beforeAll(async () => {
+    await db.execute("delete auth::session;");
+    await db.execute("delete auth::account;");
+    await db.execute("delete auth::user;");
+  });
 
   await runAdapterTest({
+    disableTests: {
+      CREATE_MODEL: false,
+      CREATE_MODEL_SHOULD_ALWAYS_RETURN_AN_ID: false,
+      FIND_MODEL: false,
+      FIND_MODEL_WITHOUT_ID: false,
+      FIND_MODEL_WITH_SELECT: false,
+      FIND_MODEL_WITH_MODIFIED_FIELD_NAME: false,
+      UPDATE_MODEL: true,
+      SHOULD_FIND_MANY: true,
+      SHOULD_FIND_MANY_WITH_WHERE: true,
+      SHOULD_FIND_MANY_WITH_OPERATORS: true,
+      SHOULD_WORK_WITH_REFERENCE_FIELDS: true,
+      SHOULD_FIND_MANY_WITH_SORT_BY: true,
+      SHOULD_FIND_MANY_WITH_LIMIT: true,
+      SHOULD_FIND_MANY_WITH_OFFSET: true,
+      SHOULD_UPDATE_WITH_MULTIPLE_WHERE: true,
+      DELETE_MODEL: true,
+      SHOULD_DELETE_MANY: true,
+      SHOULD_NOT_THROW_ON_DELETE_RECORD_NOT_FOUND: true,
+      SHOULD_NOT_THROW_ON_RECORD_NOT_FOUND: true,
+      SHOULD_FIND_MANY_WITH_CONTAINS_OPERATOR: true,
+      SHOULD_SEARCH_USERS_WITH_STARTS_WITH: true,
+      SHOULD_SEARCH_USERS_WITH_ENDS_WITH: true,
+      SHOULD_PREFER_GENERATE_ID_IF_PROVIDED: true,
+    },
     getAdapter: async (betterAuthOptions = {}) => {
       return adapter(betterAuthOptions);
     },
