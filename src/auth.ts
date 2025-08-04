ba350cd424161314f5f222becd0cb768de2d25cd
Kevin Abatan <abatan.k@gmail.com>
2025-08-04 02:34:18 +0200

    fix: bad formatting

--- src/auth.ts
+++ src/auth.ts
@@ -10,7 +10,7 @@ export const auth = betterAuth({
       account: ["userId"],
       session: ["userId", "token"],
       verification: ["identifier"],
-      organization: ["slug"]
+      organization: ["slug"],
     },
   }),
 });
